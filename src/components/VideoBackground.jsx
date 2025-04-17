8be7cc402f7db0b9fbc403186c0b4605f777f7d1
Richard Colachagua <richardcolachagua@gmail.com>
2025-04-17 00:58:34 -0400

    Implemented video background to welcome page

---
+++ src/components/VideoBackground.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const VideoBackground = ({ videoSrc, children }) => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          opacity: 0.9,
+        }}
+      >
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+      <Box
+        sx={{
+          position: "relative",
+          zIndex: 1,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          textAlign: "center",
+        }}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default VideoBackground;
--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,65 +1,45 @@
 import React from "react";
 import { Typography, Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import VideoBackground from "../components/VideoBackground";
 
 const WelcomePage: React.FC = () => {
   return (
     <Box
       sx={{
-        backgroundImage: `url("/assets/Ironman_and_Batman_fighting_each_other.png")`,
-        backgroundColor: "red",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        alignItems: "left",
         justifyContent: "center",
       }}
     >
-      <Typography
-        variant="h3"
-        align="left"
-        sx={{ fontWeight: "bold", color: "red", paddingLeft: "30px" }}
-      >
-        The Clash of the Billionaires
-      </Typography>
-      <Typography
-        variant="h3"
-        align="left"
-        sx={{ fontWeight: "bold", color: "red", paddingLeft: "30px" }}
-      >
-        Two Geniuses
-      </Typography>
-      <Typography
-        align="left"
-        sx={{
-          fontWeight: "bold",
-          color: "red",
-          fontSize: "20px",
-          paddingLeft: "30px",
-          fontFamily: "monospace",
-        }}
-      >
-        Two trauma damaged men, fighting their own demons by fighting crime
-      </Typography>
+      <VideoBackground videoSrc="assets/WelcomePageVideo.mp4">
+        <Typography
+          variant="h3"
+          align="left"
+          sx={{ fontWeight: "bold", color: "white", paddingLeft: "30px" }}
+        >
+          BILLIONAIRES, GENIUSES, PLAYBOYS, PHILANTROPISTS
+        </Typography>
 
-      <Button
-        component={Link}
-        to="/landing"
-        variant="contained"
-        sx={{
-          alignItems: "left",
-          color: "red",
-          backgroundColor: "black",
-          paddingLeft: "20px",
-          marginLeft: "30px",
-          margin: "30px",
-          width: "340px",
-        }}
-      >
-        WHO WOULD REALLY WIN IN A FIGHT?
-      </Button>
+        <Button
+          component={Link}
+          to="/landing"
+          variant="contained"
+          sx={{
+            alignItems: "left",
+            color: "red",
+            backgroundColor: "white",
+            paddingLeft: "20px",
+            marginLeft: "30px",
+            margin: "30px",
+            width: "340px",
+            borderRadius: "30px",
+          }}
+        >
+          WHO WOULD WIN IN A FIGHT?
+        </Button>
+      </VideoBackground>
     </Box>
   );
 };
